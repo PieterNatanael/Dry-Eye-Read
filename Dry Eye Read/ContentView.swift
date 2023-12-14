21c9271c1de57f64e22f13a5e197530e9bc3bc9b
PieterNatanael <pieternatanael@icloud.com>
2023-12-14 11:41:04 +0800

    Initial Commit

---
+++ Dry Eye Read/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dry Eye Read
+//
+//  Created by Pieter Yoshua Natanael on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dry Eye Read/Dry_Eye_ReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dry_Eye_ReadApp.swift
+//  Dry Eye Read
+//
+//  Created by Pieter Yoshua Natanael on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Dry_Eye_ReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
